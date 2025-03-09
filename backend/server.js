e702c1f0c2943c1d9a4acb8f0aa4c48ea909d328
Ej <elnesjani@gmail.com>
2025-03-09 20:10:56 +0800

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
+app.listen(port, () => {
     connectDB();
-    console.log("server started at http://localhost:3000");
+    console.log(`server started at http://localhost:${port}`);
 });
